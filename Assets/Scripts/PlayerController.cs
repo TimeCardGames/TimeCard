b626a202bf7d0b63f9f8f99a16a8a1babd96677b
Justin Dao <tjustindao@gmail.com>
2016-05-15 16:36:04 -0700

    Add player object

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+    public List<GameObject> hand;
+
+	// Use this for initialization
+	void Start () {
+        if (hand != null)
+        {
+            Debug.Log("Not null hand");
+            var xVal = -1;
+            foreach (var card in hand)
+            {
+                var cardController = card.GetComponent<CardController>();
+                cardController.defaultPosition = new Vector3(xVal, 0.0f, 0.0f);
+                xVal += 2;
+                Instantiate(card, cardController.defaultPosition, card.transform.rotation);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
