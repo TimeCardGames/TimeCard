bf56e7afc791825da75ee641db071eb553ae51ab
Justin Dao <tjustindao@gmail.com>
2016-05-15 14:27:37 -0700

    Add moveable minion

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -6,9 +6,11 @@ public class CardController : MonoBehaviour {
     public Camera cam;
     public bool selected = false;
     public Vector3 defaultPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    public CardType cardType;
+    public GameObject minion;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
 	    if (cam == null)
         {
@@ -28,6 +30,11 @@ public class CardController : MonoBehaviour {
         }        
 	}
 
+    void Update()
+    {
+
+    } 
+
     void OnMouseOver()
     {
         if (!selected)
@@ -35,6 +42,7 @@ public class CardController : MonoBehaviour {
             if (Input.GetMouseButtonDown(0))
             {
                 selected = true;
+                GetComponent<SpriteRenderer>().sortingOrder = 5;
             }
         }
         else
@@ -43,12 +51,40 @@ public class CardController : MonoBehaviour {
             var mouseY = cam.ScreenToWorldPoint(Input.mousePosition).y;
             var borderY = border.transform.position.y;
 
-            if (Input.GetMouseButtonDown(0) && mouseY < borderY)
+            if (Input.GetMouseButtonDown(0))
             {
-                selected = false;
-                GetComponent<Rigidbody2D>().MovePosition(defaultPosition);
+                if (mouseY < borderY)
+                {
+                    selected = false;
+                    GetComponent<Rigidbody2D>().MovePosition(defaultPosition);
+                    GetComponent<SpriteRenderer>().sortingOrder = 0;
+                }
+                else
+                {
+                    // Card is played
+                    if (this.cardType == CardType.Minion)
+                    { 
+                        minion = new GameObject();
+                        minion.name = "Minion";
+                        minion.AddComponent<SpriteRenderer>();
+                        minion.AddComponent<Rigidbody2D>();
+                        minion.AddComponent<BoxCollider2D>();
+                        minion.AddComponent<MinionController>();
+                        Sprite sprite = Resources.Load<Sprite>("Sprites/Minion");
+
+                        var sr = minion.GetComponent<SpriteRenderer>();
+                        sr.sprite = sprite;
+                        sr.sortingLayerName = "Foreground";
+                        sr.sortingOrder = 1;
+                        minion.GetComponent<Rigidbody2D>().isKinematic = true;
+                        Vector3 spawnPosition = new Vector3(0, 3, 0);
+                        minion.transform.position = spawnPosition;
+                        selected = false;
+                        Destroy(gameObject);
+                    }
+                }
             }
-        }            
+        }
     }
             
 }
---
+++ Assets/Scripts/MinionController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionController : MonoBehaviour {
+
+    public Camera cam;
+    public Vector3 targetPosition;
+    public float speed = 2.0f;
+    public bool selected;
+    private bool buttonDown;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+
+        this.targetPosition = this.transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            buttonDown = false;
+        }
+
+        if (!buttonDown)
+        {
+            if (selected && Input.GetMouseButtonDown(0))
+            {
+                Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+                this.targetPosition = new Vector3(rawPosition.x, rawPosition.y, 0.0f);
+                selected = false;
+                var sr = GetComponent<SpriteRenderer>();
+                sr.color = new Color(1, 1, 1, 1);
+            }
+        }        
+    }
+	
+	// FixedUpdate is called once per physics timestep
+	void FixedUpdate ()
+    {
+        
+        var rigidbody2d = GetComponent<Rigidbody2D>();
+        rigidbody2d.MovePosition(Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed));
+    }
+
+    void OnMouseOver()
+    {
+        if (!selected)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                selected = true;
+                buttonDown = true;
+                var sr = GetComponent<SpriteRenderer>();
+                sr.color = new Color(0.5f, 0.5f, 0.5f, 1);
+                GetComponent<SpriteRenderer>().sortingOrder = 5;
+            }
+        }
+    }
+}
