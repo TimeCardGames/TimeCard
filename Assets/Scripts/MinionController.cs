9f1383c6105dc93ae8c39eeeb78e71fa5b68e701
Justin Dao <tjustindao@gmail.com>
2016-05-15 17:42:22 -0700

    Add text

--- Assets/Scripts/MinionController.cs
+++ Assets/Scripts/MinionController.cs
@@ -5,12 +5,17 @@ public class MinionController : MonoBehaviour {
 
     public Camera cam;
     public Vector3 targetPosition;
+    public int attack;
+    public int health;
     public float speed = 2.0f;
     public bool selected;
+    public GameObject attackText;
+    public GameObject healthText;
+
     private bool buttonDown;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         if (cam == null)
         {
@@ -25,6 +30,21 @@ public class MinionController : MonoBehaviour {
         Vector3 spawnPosition = new Vector3(0, 3, 0);
         transform.position = spawnPosition;
         selected = false;
+
+        var currPos = gameObject.transform.position;
+
+        // Initialize Text
+        attackText.GetComponent<TextMesh>().text = attack.ToString();
+        attackText.GetComponent<MeshRenderer>().sortingLayerName = "Foreground";
+        attackText.GetComponent<MeshRenderer>().sortingOrder = 2;
+        attackText.transform.position = new Vector3(currPos.x - 0.5f, currPos.y - 0.5f, currPos.z);
+        attackText = (GameObject)Instantiate(attackText, attackText.transform.position, attackText.transform.rotation);
+
+        healthText.GetComponent<TextMesh>().text = health.ToString();
+        healthText.GetComponent<MeshRenderer>().sortingLayerName = "Foreground";
+        healthText.GetComponent<MeshRenderer>().sortingOrder = 2;
+        healthText.transform.position = new Vector3(currPos.x + 0.5f, currPos.y - 0.5f, currPos.z);
+        healthText = (GameObject)Instantiate(healthText, healthText.transform.position, healthText.transform.rotation);
     }
 
     void Update()
@@ -58,6 +78,7 @@ public class MinionController : MonoBehaviour {
     {
         var rigidbody2d = GetComponent<Rigidbody2D>();
         rigidbody2d.MovePosition(Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed));
+        MoveText();
     }
 
     void OnMouseOver()
@@ -88,5 +109,12 @@ public class MinionController : MonoBehaviour {
             var sr = GetComponent<SpriteRenderer>();
             sr.color = new Color(1.0f, 1.0f, 1.0f, 1);
         }
+    }
+
+    void MoveText()
+    {
+        var currPos = GetComponent<Rigidbody2D>().transform.position;
+        attackText.transform.position = new Vector3(currPos.x - 0.5f, currPos.y - 0.5f, currPos.z);
+        healthText.transform.position = new Vector3(currPos.x + 0.5f, currPos.y - 0.5f, currPos.z);
     }
 }
